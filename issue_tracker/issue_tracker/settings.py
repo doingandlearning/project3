792765172d4b0de4e4eac844ab3ca7658a2aeda1
doingandlearning <kevin@doingandlearning.com>
2018-04-13 19:12:16 +0100

    Customised the admin for polls

--- issue_tracker/issue_tracker/settings.py
+++ issue_tracker/issue_tracker/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'issue_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
