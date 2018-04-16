88a22c3a3e0b10ca38645d9857c632aa19966c28
doingandlearning <kevin@doingandlearning.com>
2018-04-16 19:54:44 +0100

    Added Bulma dependencies for front end development.

---
+++ issue_tracker/issue_tracker/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+# Create your views here.
+def index(request):
+    return render(request, 'bulma_base.html')
+
